cf93e0b24d9e6c5d6636f7492ef100f99186d54e
Atul Rauthan <atulrauthan7@gmail.com>
2020-06-04 11:37:51 +0530

    Added routing

---
+++ src/app/app-routing/routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
+import { ContactComponent } from '../contact/contact.component';
+import { MenuComponent } from '../menu/menu.component';
+import { DishDetailComponent } from '../dish-detail/dish-detail.component';
+
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'menu', component: MenuComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,18 @@ import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
+
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
 
 import { DishService } from './services/dish.service';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -24,12 +30,16 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishDetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    AppRoutingModule,
     MatToolbarModule,
     MatListModule,
     MatGridListModule,
